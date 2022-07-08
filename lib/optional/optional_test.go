c68c00e9518708f92e7ce05394e59ef781ac63f2
Grayson Hansard <grayson.hansard@gmail.com>
2022-07-08 14:44:41 -0400

    Add `optional` type

---
+++ lib/optional/optional_test.go
@@ -0,0 +1,87 @@
+package optional_test
+
+import (
+	"grayson/cct/lib/optional"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	o := optional.Empty[*int]()
+	if o.HasValue {
+		t.Error("Optional unexpected has value")
+	}
+	v, ok := o.Get()
+	if v != nil {
+		t.Error("Expected nil value in optional")
+	}
+
+	if ok {
+		t.Error("Expected `ok` to be false")
+	}
+}
+
+func TestFilled(t *testing.T) {
+	i := 42
+	o := optional.Of(&i)
+	if !o.HasValue {
+		t.Error("Expected optional to have value")
+	}
+	v, ok := o.Get()
+	if v == nil {
+		t.Error("Expected optional to have valid pointer")
+	}
+
+	deref := *v
+	if deref != 42 {
+		t.Errorf("Unexpected value in optional: %d (expected %d)", deref, 42)
+	}
+
+	if !ok {
+		t.Error("Expected `ok` to be true")
+	}
+}
+
+func TestValueType(t *testing.T) {
+	o := optional.Of(42)
+	if !o.HasValue {
+		t.Error("Expected optional to have value")
+	}
+	v, ok := o.Get()
+	if v != 42 {
+		t.Errorf("Unexpected value in optional: %d (expected %d)", v, 42)
+	}
+
+	if !ok {
+		t.Error("Expected `ok` to be true")
+	}
+}
+
+func TestValueTypeInNillableApi(t *testing.T) {
+	o := optional.OfNonZero(42)
+	if !o.HasValue {
+		t.Error("Expected optional to have value")
+	}
+	v, ok := o.Get()
+	if v != 42 {
+		t.Errorf("Unexpected value in optional: %d (expected %d)", v, 42)
+	}
+
+	if !ok {
+		t.Error("Expected `ok` to be true")
+	}
+}
+
+func TestZeroValueOfValueTypeInNonZeroApi(t *testing.T) {
+	o := optional.OfNonZero(0)
+	if o.HasValue {
+		t.Error("Expected optional to not have value")
+	}
+	v, ok := o.Get()
+	if v != 0 {
+		t.Errorf("Unexpected value in optional: %d (expected %d)", v, 0)
+	}
+
+	if ok {
+		t.Error("Expected `ok` to be false")
+	}
+}
