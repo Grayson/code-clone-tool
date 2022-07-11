c9aea49e573081a3f8d142a215c9ca1971200020
Grayson Hansard <grayson.hansard@gmail.com>
2022-07-11 12:41:33 -0400

    Fix spacing

--- lib/GitApi/GitClient.go
+++ lib/GitApi/GitClient.go
@@ -17,7 +17,7 @@ func CreateGitClient(log *log.Logger) *GitClient {
 }
 
 func (gc *GitClient) Clone(gitUrl string, path string) (string, error) {
-	gc.log.Println("Executing `git clone ", gitUrl, path, "`")
+	gc.log.Println("Executing `git clone", gitUrl, path, "`")
 	cmd := exec.Command("git", "clone", gitUrl, path)
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
@@ -29,7 +29,7 @@ func (gc *GitClient) Clone(gitUrl string, path string) (string, error) {
 }
 
 func (gc *GitClient) Pull(destinationDir string) (string, error) {
-	gc.log.Println("Executing `git pull` in ", destinationDir)
+	gc.log.Println("Executing `git pull` in", destinationDir)
 	cmd := exec.Command("git", "pull")
 	cmd.Dir = destinationDir
 	var stdout bytes.Buffer
