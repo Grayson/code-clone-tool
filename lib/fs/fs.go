ae45f33c8266c38a9abe61717afa5e020b6056f7
Grayson <grayson.hansard@gmail.com>
2022-07-21 00:21:20 +0000

    Move path info to package to hoist concerns to a testable state

--- lib/fs/fs.go
+++ lib/fs/fs.go
@@ -1,5 +1,21 @@
 package fs
 
+type PathExistential int
+
+const (
+	Exists PathExistential = iota
+	DoesNotExist
+)
+
+type PathType int
+
+const (
+	None PathType = iota
+	IsFile
+	IsDirectory
+)
+
 type Fs interface {
 	ChangeWorkingDirectory(string) error
+	Info(path string) (PathExistential, PathType)
 }
--- lib/fs/os.go
+++ lib/fs/os.go
@@ -1,9 +1,23 @@
 package fs
 
-import "os"
+import (
+	"os"
+)
 
 type OsFs struct{}
 
 func (o OsFs) ChangeWorkingDirectory(path string) error {
 	return os.Chdir(path)
+}
+
+func (o OsFs) Info(path string) (PathExistential, PathType) {
+	info, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		return DoesNotExist, None
+	}
+
+	if info.IsDir() {
+		return Exists, IsDirectory
+	}
+	return Exists, IsFile
 }
