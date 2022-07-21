5342caeae6186482068fbf04f5adcb6f17e3e50b
Grayson <grayson.hansard@gmail.com>
2022-07-21 00:23:14 +0000

    Remove test for removed function

--- main_test.go
+++ main_test.go
@@ -335,31 +335,6 @@ func Test_mapActions(t *testing.T) {
 	}
 }
 
-func Test_discernPathInfo(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name  string
-		args  args
-		want  lib.PathExistential
-		want1 lib.PathType
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := discernPathInfo(tt.args.path)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("discernPathInfo() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("discernPathInfo() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
 // Test implementations
 
 type TestGit struct {
