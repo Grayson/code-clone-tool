577b7bc5b1282eeb547b2da337f55d354e52bbc2
Grayson <grayson.hansard@gmail.com>
2022-07-28 03:01:24 +0000

    Load yaml config using struct tags

--- lib/env.go
+++ lib/env.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"fmt"
 	"reflect"
 
 	"gopkg.in/yaml.v3"
@@ -32,19 +31,6 @@ type EnvFile struct {
 	ApiUrl              string `yaml:"api_url"`
 	WorkingDirectory    string `yaml:"working_directory"`
 }
-
-type envFileWrapper struct {
-	EnvFile EnvFile
-	Status  LoadStatus
-}
-
-type loadYamlEnvFile func() *envFileWrapper
-
-const (
-	Unloaded LoadStatus = iota
-	Loaded
-	FailedToLoad
-)
 
 func LoadEnvironmentVariables(get GetEnvVar) *Env {
 	env := Env{}
@@ -97,96 +83,4 @@ func (left *Env) Merge(right *Env) *Env {
 		}
 	}
 	return &combined
-}
-
-func (e *envFileWrapper) lookup(key EnvironmentVariableKey) string {
-	switch key {
-	case PersonalAccessToken:
-		return e.EnvFile.PersonalAccessToken
-	case ApiUrl:
-		return e.EnvFile.ApiUrl
-	case WorkingDirectory:
-		return e.EnvFile.WorkingDirectory
-	}
-	panic(fmt.Sprintf("Unknown key: %v", key))
-}
-
-func loadEnvFile(reader ReadYamlFile) *envFileWrapper {
-	var file EnvFile
-	bytes, err := reader()
-	if err != nil {
-		return &envFileWrapper{
-			Status: FailedToLoad,
-		}
-	}
-	if err := yaml.Unmarshal(bytes, &file); err != nil {
-		return &envFileWrapper{
-			Status: FailedToLoad,
-		}
-	}
-	return &envFileWrapper{
-		EnvFile: file,
-		Status:  Loaded,
-	}
-}
-
-func getPersonalAccessToken(get GetEnvVar, load loadYamlEnvFile) string {
-	value, ok := get(string(PersonalAccessToken))
-	if ok {
-		return value
-	}
-	if env := load(); env.Status == Loaded {
-		return env.EnvFile.PersonalAccessToken
-	}
-
-	return ""
-}
-
-func getOrganizationUrl(get GetEnvVar, load loadYamlEnvFile) string {
-	value, ok := get(string(ApiUrl))
-	if ok {
-		return value
-	}
-	if env := load(); env.Status == Loaded {
-		return env.EnvFile.ApiUrl
-	}
-
-	return ""
-}
-
-func getStringConfig(get GetEnvVar, load loadYamlEnvFile, key EnvironmentVariableKey) string {
-	value, ok := get(string(key))
-	if ok {
-		return value
-	}
-	if env := load(); env.Status == Loaded {
-		return env.lookup(key)
-	}
-
-	return ""
-}
-
-func NewEnv(get GetEnvVar, read []ReadYamlFile) *Env {
-	envFile := &envFileWrapper{}
-	loader := func() *envFileWrapper {
-		if envFile.Status == Loaded {
-			return envFile
-		}
-
-		for _, x := range read {
-			tmp := loadEnvFile(x)
-			if tmp.Status != Loaded {
-				continue
-			}
-			envFile = tmp
-			break
-		}
-		return envFile
-	}
-
-	return &Env{
-		PersonalAccessToken: getPersonalAccessToken(get, loader),
-		ApiUrl:              getOrganizationUrl(get, loader),
-		WorkingDirectory:    getStringConfig(get, loader, WorkingDirectory),
-	}
 }
