d3158151495dc3d926afbfc9bc3c7b08afd27981
Grayson <grayson.hansard@gmail.com>
2022-07-28 00:49:01 +0000

    Add API to execute commands in a directory

--- lib/shell/shell.go
+++ lib/shell/shell.go
@@ -6,7 +6,22 @@ import (
 )
 
 func Do(first string, args ...string) (string, error) {
+	return In("").Do(first, args...)
+}
+
+type impl struct {
+	destination string
+}
+
+func In(destination string) *impl {
+	return &impl{
+		destination: destination,
+	}
+}
+
+func (i *impl) Do(first string, args ...string) (string, error) {
 	cmd := exec.Command(first, args...)
+	cmd.Dir = i.destination
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
 	err := cmd.Run()
