f2e9bb94270c8c3b46359a3d186589f5bc5f942f
Grayson Hansard <grayson.hansard@gmail.com>
2023-05-28 19:03:20 -0400

    Move git test mock

--- main_test.go
+++ main_test.go
@@ -289,21 +289,6 @@ func Test_determineConfigPath(t *testing.T) {
 
 // Test implementations
 
-type TestGit struct {
-	DidClone bool
-	DidPull  bool
-}
-
-func (g *TestGit) Clone(gitUrl string, path string) (string, error) {
-	g.DidClone = true
-	return "stdout", nil
-}
-
-func (g *TestGit) Pull(destinationDir string) (string, error) {
-	g.DidPull = true
-	return "stdout", nil
-}
-
 type TestApi struct {
 	Error         error
 	ErrorResponse *githubapi.GithubOrgReposErrorResponse
